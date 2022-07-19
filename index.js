a438b76c55aa008cd88cb995be957c08fc95a0ae
kyem-asa <turtlemoons@protonmail.com>
2022-07-19 16:57:29 -0500

    Add generation

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 fetch("https://ghibliapi.herokuapp.com/films")
     .then(res => res.json()) // parse response as JSON
-    .then(data => {
-      // console.log(data[Math.floor(Math.random() * data.length)].image)
-      // document.querySelector('img').src = data.message
-      document.querySelector('.mainContent__subheading').innerText = data[Math.floor(Math.random() * data.length)].title
+    .then(data => {console.log(data)
 
-      document.querySelector('img').src = data[0].image;
+      document.querySelector('.mainContent__btn').addEventListener('click', function(){
+        
+     
+      let currentMovie = Math.floor(Math.random() * data.length)
+
+      console.log(currentMovie)
+      // // console.log(data[Math.floor(Math.random() * data.length)].image)
+      // // document.querySelector('img').src = data.message
+      document.querySelector('.mainContent__subheading').innerText = data[currentMovie].title;
+      document.querySelector('img').src = data[currentMovie].image;
+      document.querySelector('.mainContent__bodyText').innerText = data[currentMovie].description;
     })
 
     .catch(err => {
         console.log(`error ${err}`)
     });
+  })
