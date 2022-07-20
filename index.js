1a6b5004a6046032f1a305ebf09ad48da43188bc
kyem-asa <turtlemoons@protonmail.com>
2022-07-20 09:31:05 -0500

    Add color mode toggle

--- index.js
+++ index.js
@@ -19,3 +19,20 @@ fetch("https://ghibliapi.herokuapp.com/films")
         console.log(`error ${err}`)
     });
   })
+
+  document.querySelector('.darkToggle').addEventListener('click', function(){
+    console.log('works')
+    document.querySelector('body').style.color = 'hsl(160, 100%, 99%)'
+    document.querySelector('body').style.backgroundColor = '#1B1B1B'
+
+    document.querySelector('.darkToggle').style.display = 'none'
+    document.querySelector('.lightToggle').style.display = 'flex'
+  })
+
+document.querySelector('.lightToggle').addEventListener('click', function(){
+  document.querySelector('body').style.color = 'hsl(164, 33%, 16%)'
+  document.querySelector('body').style.backgroundColor = 'hsl(160, 100%, 99%)'
+
+  document.querySelector('.lightToggle').style.display = 'none'
+  document.querySelector('.darkToggle').style.display = 'flex'
+})
